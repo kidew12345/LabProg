41f0239446024e0559d49572e636f280e10fef3c
kidew12345 <58657504+kidew12345@users.noreply.github.com>
2019-12-09 14:22:53 +0700

    Add files via upload

---
+++ tesimage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import { AppRegistry, Image } from 'react-native'
+
+render(){
+	return(
+<Image source = {{uri :'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ad/LetterD.svg/1200px-LetterD.svg.png')} />)
+}
+}
+export default ImagesExample
