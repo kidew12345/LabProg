b58d04fa3b9bcababeea59bf8ddb6ab5ef3bcf18
kidew12345 <58657504+kidew12345@users.noreply.github.com>
2019-12-09 16:36:15 +0700

    Add files via upload

---
+++ ImagesExample1.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react'
+import { Image } from 'react-native'
+const ImagesExample = () => (
+<Image source = {require('D:/XAMPP/htdocs/cobareact/latihan/chapter4/Jeruklegi.jpg')} />)
+export default ImagesExample
